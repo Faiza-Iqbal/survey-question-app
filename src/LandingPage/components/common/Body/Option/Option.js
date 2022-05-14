248b82a0c907603f8ec1c2d52bba5d5472b93b4a
unknown <faiza.iqbal@emumba.com>
2022-05-14 12:19:05 +0500

    test

--- src/LandingPage/components/common/Body/Option/Option.js
+++ src/LandingPage/components/common/Body/Option/Option.js
@@ -1,17 +1,26 @@
 import React from "react";
 // Adding Style
 import "./Option.css";
+const getInput = (inputValue) =>
+  console.log("inputValue", inputValue.target.value);
 const Option = (
   { data, selected } //Accepting props, Destructed option to improve code readability when accessing its properties
 ) => (
   <>
     {/* Adding active style on option selection */}
     <div className={selected ? "activeOption" : "defaultOption"}>
-      <span> {data.value} </span>
+      <span> {data.title} </span>
       {data.label}
     </div>
     {/* Conditional rendering of input field in last option */}
-    {data.inputBox && <input className="otherInput" type="text" name="name" />}
+    {data.inputBox && (
+      <input
+        onChange={(value) => getInput(value)}
+        className="otherInput"
+        type="text"
+        name="name"
+      />
+    )}
   </>
 );
 
